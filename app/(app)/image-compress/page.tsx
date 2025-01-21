25e5227aea3e2c123b22400fbed9a307962319d0
my 1st repository <supriyadhal50@gmail.com>
2025-01-22 01:42:38 +0530

    add image compression functionality; implement image upload and download features with Cloudinary integration

--- app/(app)/image-compress/page.tsx
+++ app/(app)/image-compress/page.tsx
@@ -1,7 +1,170 @@
-import React from "react";
+"use client";
 
-const page = () => {
-  return <div>image compressor</div>;
+import { filesize } from "filesize";
+import { CldImage } from "next-cloudinary";
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import { toast } from "react-toastify";
+
+const ImageCompress = () => {
+  const [uploadedImage, setUploadedImage] = useState<string | null>(null);
+  const [isUploading, setIsUploading] = useState(false);
+  const [isTransfering, setIsTransfering] = useState(false);
+  const imageRef = useRef<HTMLImageElement>(null);
+  const [selectedFormat, setSelectedFormat] = useState("png");
+  const [width, setWidth] = useState(0);
+  const [height, setHeight] = useState(0);
+
+  useEffect(() => {
+    if (uploadedImage) {
+      setIsTransfering(true);
+    }
+  }, [uploadedImage]);
+
+  const handleFileChange = async (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const file = event.target.files?.[0];
+    if (!file) return;
+    setIsUploading(true);
+
+    const formData = new FormData();
+    formData.append("file", file);
+
+    try {
+      const response = await fetch("/api/image-compress", {
+        method: "POST",
+        body: formData,
+      });
+
+      if (!response.ok) {
+        toast.error("Failed to upload image");
+        throw new Error(`Failed to upload image: ${response.statusText}`);
+      }
+
+      const data = await response.json();
+
+      setUploadedImage(data.publicId);
+      setWidth(data.width);
+      setHeight(data.height);
+      toast.success("Image uploaded successfully");
+    } catch (error) {
+      console.error(error);
+      toast.error("Failed to upload image. Please try again.");
+    } finally {
+      setIsUploading(false);
+    }
+  };
+
+  const handleDownload = () => {
+    if (!imageRef.current) return;
+
+    fetch(imageRef.current.src)
+      .then((Response) => Response.blob())
+      .then((blob) => {
+        //we are trying to control the blob by creating a URL out of it
+        const url = window.URL.createObjectURL(blob);
+        //then we are creating a link by the anchor tag <a></a>
+        const link = document.createElement("a");
+        //<a href = {url}> </a>
+        link.href = url;
+        //<a href = {url} download = "image.png"> </a>
+        link.download = `${selectedFormat
+          .replace(/\s+/g, "_")
+          .toLowerCase()}.png`;
+        //we are appending the link(<a></a>) to the body
+        document.body.appendChild(link);
+        //we are simulating a click on the link
+        link.click();
+        //we are removing the link
+        document.body.removeChild(link);
+        //we are revoking the URL
+        window.URL.revokeObjectURL(url);
+        //we are removing the link
+        document.body.removeChild(link);
+      });
+  };
+
+  // const formatFileSize = useCallback((size: number) => {
+  //   return filesize(size);
+  // }, []);
+
+  // const compressionPercentage = Math.round(
+  //   (1 - Number(video.compressedSize) / Number(video.originalSize)) * 100
+  // );
+
+  return (
+    <div className="container mx-auto p-4 max-w-4xl">
+      <h1 className="text-3xl font-bold mb-6 text-center"> Image compressor</h1>
+
+      <div className="card">
+        <div className="card-body">
+          <h2 className="card-title mb-4">Upload an Image</h2>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Choose an Image file</span>
+            </label>
+            <input
+              type="file"
+              className="file-input file-input-bordered file-input-primary w-full"
+              onChange={handleFileChange}
+            />
+          </div>
+
+          {isUploading && (
+            <div className="mt-4">
+              <progress className="progress progress-primary w-full"></progress>
+            </div>
+          )}
+
+          {uploadedImage && (
+            <div className="mt-6">
+              <div className="mt-6 relative">
+                <h3 className="text-lg font-semibold mb-2">Preview:</h3>
+                <div className="flex justify-center">
+                  {isTransfering && (
+                    <div className="absolute inset-0 flex items-center justify-center bg-base-100 bg-opacity-50 z-10">
+                      <span className="loading loading-spinner loading-lg"></span>
+                    </div>
+                  )}
+
+                  <CldImage
+                    src={uploadedImage}
+                    width={width}
+                    height={height}
+                    sizes="100vw"
+                    alt="Transfered Image"
+                    gravity="auto"
+                    ref={imageRef}
+                    onLoad={() => setIsTransfering(false)}
+                  />
+                </div>
+              </div>
+
+              <div className="flex justify-between mt-6">
+                <div>
+                  <label htmlFor="format">Select Format</label>
+                  <select
+                    className="select select-bordered w-full"
+                    value={selectedFormat}
+                    onChange={(e) => setSelectedFormat(e.target.value)}
+                  >
+                    <option value="jpeg">JPEG</option>
+                    <option value="png">PNG</option>
+                    <option value="webp">WEBP</option>
+                  </select>
+                </div>
+                <div>
+                  <button className="btn btn-primary" onClick={handleDownload}>
+                    Download for {selectedFormat}{" "}
+                  </button>
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default page;
+export default ImageCompress;
---
+++ app/api/image-compress/route.ts
@@ -0,0 +1,85 @@
+import { auth } from "@clerk/nextjs/server";
+import { v2 as cloudinary } from "cloudinary";
+import { NextRequest, NextResponse } from "next/server";
+
+// Configuration
+cloudinary.config({
+  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET, // Click 'View API Keys' above to copy your API secret
+});
+
+interface CloudinaryUploadResult {
+  public_id: string;
+  [key: string]: unknown; //this is a dynamic key means we have more stuff in this object
+}
+
+export async function POST(request: NextRequest) {
+  const { userId } = await auth();
+
+  if (!userId) {
+    return NextResponse.json(
+      { error: "You are not authorized to upload images" },
+      { status: 401 }
+    );
+  }
+
+  try {
+    // Get the file from the request
+    const formData = await request.formData();
+
+    const file = formData.get("file") as File | null;
+    const originalSize = formData.get("originalSize") as string;
+
+    console.log(originalSize);
+
+    if (!file) {
+      return NextResponse.json({ error: "No file found" }, { status: 400 });
+    }
+
+    // Upload the file to Cloudinary is a 3-step process:
+    // 1. convert the file to an array buffer
+    const bytes = await file.arrayBuffer();
+    //2. convert the array buffer to a buffer
+    const buffer = Buffer.from(bytes);
+
+    //3. upload the buffer to Cloudinary and along with an error callback handling
+    const result = await new Promise<CloudinaryUploadResult>(
+      (resolve, reject) => {
+        cloudinary.uploader
+          .upload_stream(
+            {
+              folder: "nextjs-compressed-image-upload",
+              transformation: [{ quality: "auto", fetch_format: "auto" }],
+            },
+            (error, result) => {
+              if (error) {
+                reject(error);
+              } else {
+                resolve(result as CloudinaryUploadResult);
+              }
+            }
+          )
+          .end(buffer);
+      }
+    );
+
+    console.log(result);
+
+    return NextResponse.json(
+      {
+        publicId: result.public_id,
+        width: result.width,
+        height: result.height,
+        compressedSize: result.bytes,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log("Error uploading image", error);
+    return NextResponse.json(
+      { error: "Error uploading image" },
+      { status: 500 }
+    );
+  }
+}
